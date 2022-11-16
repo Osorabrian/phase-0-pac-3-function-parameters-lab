2937985fe4bd21502f75c834edf5632ca2209112
Osorabrian <osorabrian@gmail.com>
2022-11-16 13:11:25 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name,language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+introduction("Aki");
+introductionLanguage("Aki", "Ember.js");
+introductionWithLanguageOptional("Gracie")
+introductionWithLanguageOptional("Gracie","Python")
+
+
+
